8da6b5271c96a2d0a598ee2e02f43658bdd53f4b
tokorom <tokorom@gmail.com>
2011-11-27 19:42:47 +0900

    first commit

---
+++ ChameleonUIKitDemo/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  ChameleonUIKitDemo
+//
+//  Created by Yuta Tokoro on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
